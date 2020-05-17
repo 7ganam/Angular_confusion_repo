3b8b7df87a3268a1e18154c005ddc17a322bc15e
ganam <7ganam@gmail.com>
2020-05-18 01:14:19 +0200

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,10 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import 'hammerjs';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
@@ -44,7 +47,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 
 
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
